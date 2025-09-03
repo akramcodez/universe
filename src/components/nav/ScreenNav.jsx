0b5d68c1aa672e5f760d2fcde245dce4af5792d9
akramcodez <skcodewizard786@gmail.com>
2025-09-03 21:12:10 +0530

    working on menu

--- src/components/nav/ScreenNav.jsx
+++ src/components/nav/ScreenNav.jsx
@@ -1,46 +1,201 @@
-import React from 'react';
-
 const ScreenNav = () => {
   return (
-    <div id="fullScreen" className="py-60 h-screen w-full absolute bg-black">
-      <div className="">
-        <div className="link border-t-1">
-          <h1 className="font-[universe500] text-[8vw] text-center uppercase leading-[6.5vw] pt-5.5">
+    <div
+      id="fullScreen"
+      className="overflow-hidden h-screen w-full absolute bg-black"
+    >
+      <div className="flex w-full p-1.5 justify-between items-start">
+        <div className="">
+          <div className="w-25">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-full h-full"
+              fill="white"
+              viewBox="0 0 103 44"
+              aria-hidden="true"
+            >
+              <g stroke="none" transform="translate(6,4)">
+                <rect x="0" y="0" width="8" className="h-full" />
+                <rect x="0" y="34" width="30" height="6" />
+                <rect x="22" y="0" width="8" className="h-full" />
+
+                <g transform="translate(35,0)">
+                  <rect x="0" y="0" width="10" className="h-full" />
+                  <polygon points="9.6,0   36,0   36,7.2   16.8,7.2   36,31.2   28,31.2   9.6,9.6" />
+                  <rect x="28" y="0" width="8" className="h-full" />
+                </g>
+
+                <g transform="translate(76,0)">
+                  <rect x="0" y="0" width="40" height="6" />
+                  <rect x="6.5" y="0" width="8" className="h-full" />
+                  <rect x="0" y="34" width="40" height="6" />
+                  <polygon points="12,0 16,0 10,6 12,6" />
+                </g>
+              </g>
+            </svg>
+          </div>
+        </div>
+        <div className="h-13 w-15 bg-red-500">
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+      <div className="pt-27">
+        <div className="link relative border-t-[1px] cursor-pointer">
+          <h1 className="font-[universe500] text-[7.5vw] text-center uppercase leading-[6.5vw] pt-4">
             Projects
           </h1>
-          <div className="absolute flex">
-            <div>
-              <h2>Pour Tout Voir</h2>
-              <img src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"></img>
-              <h2>Pour Tout Voir</h2>
-              <img src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"></img>
+          <div className="movelink absolute top-0 flex gap-4 bg-[#C084FC] text-black">
+            <div className="moveX flex items-center gap-4">
+              <h2 className="whitespace-nowrap font-[universe500] text-[7.5vw] text-center uppercase leading-[6.5vw] pt-4">
+                Pour Tout Voir
+              </h2>
+              <img
+                className="h-20 w-56 object-cover shrink-0 rounded-full"
+                src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+              ></img>
+              <h2 className="whitespace-nowrap font-[universe500] text-[7.5vw] text-center uppercase leading-[6.5vw] pt-4">
+                Pour Tout Voir
+              </h2>
+              <img
+                className="h-20 w-56 object-cover shrink-0 rounded-full"
+                src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+              ></img>
             </div>
-            <div>
-              <h2>Pour Tout Voir</h2>
-              <img src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"></img>
-              <h2>Pour Tout Voir</h2>
-              <img src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"></img>
+            <div className="moveX flex items-center gap-4">
+              <h2 className="whitespace-nowrap font-[universe500] text-[7.5vw] text-center uppercase leading-[6.5vw] pt-4">
+                Pour Tout Voir
+              </h2>
+              <img
+                className="h-20 w-56 object-cover shrink-0 rounded-full"
+                src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+              ></img>
+              <h2 className="whitespace-nowrap font-[universe500] text-[7.5vw] text-center uppercase leading-[6.5vw] pt-4">
+                Pour Tout Voir
+              </h2>
+              <img
+                className="h-20 w-56 object-cover shrink-0 rounded-full"
+                src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+              ></img>
             </div>
           </div>
         </div>
-        <div className="link border-t-1">
-          <h1 className="font-[universe500] text-[8vw] text-center uppercase leading-[6.5vw] pt-5.5">
-            agence
+        <div className="link relative border-t-[1px] cursor-pointer">
+          <h1 className="font-[universe500] text-[7.5vw] text-center uppercase leading-[6.5vw] pt-4">
+            Projects
           </h1>
-          {/* <div>
-            <div>
-              <h2>Pour Tout Voir</h2>
-              <img src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"></img>
-              <h2>Pour Tout Voir</h2>
-              <img src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"></img>
+          <div className="movelink absolute top-0 flex gap-4 bg-[#C084FC] text-black">
+            <div className="moveX flex items-center gap-4">
+              <h2 className="whitespace-nowrap font-[universe500] text-[7.5vw] text-center uppercase leading-[6.5vw] pt-4">
+                Pour Tout Voir
+              </h2>
+              <img
+                className="h-20 w-56 object-cover shrink-0 rounded-full"
+                src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+              ></img>
+              <h2 className="whitespace-nowrap font-[universe500] text-[7.5vw] text-center uppercase leading-[6.5vw] pt-4">
+                Pour Tout Voir
+              </h2>
+              <img
+                className="h-20 w-56 object-cover shrink-0 rounded-full"
+                src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+              ></img>
             </div>
-            <div>
-              <h2>Pour Tout Voir</h2>
-              <img src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"></img>
-              <h2>Pour Tout Voir</h2>
-              <img src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"></img>
+            <div className="moveX flex items-center gap-4">
+              <h2 className="whitespace-nowrap font-[universe500] text-[7.5vw] text-center uppercase leading-[6.5vw] pt-4">
+                Pour Tout Voir
+              </h2>
+              <img
+                className="h-20 w-56 object-cover shrink-0 rounded-full"
+                src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+              ></img>
+              <h2 className="whitespace-nowrap font-[universe500] text-[7.5vw] text-center uppercase leading-[6.5vw] pt-4">
+                Pour Tout Voir
+              </h2>
+              <img
+                className="h-20 w-56 object-cover shrink-0 rounded-full"
+                src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+              ></img>
             </div>
-          </div> */}
+          </div>
+        </div>
+        <div className="link relative border-t-[1px] cursor-pointer">
+          <h1 className="font-[universe500] text-[7.5vw] text-center uppercase leading-[6.5vw] pt-4">
+            Projects
+          </h1>
+          <div className="movelink absolute top-0 flex gap-4 bg-[#C084FC] text-black">
+            <div className="moveX flex items-center gap-4">
+              <h2 className="whitespace-nowrap font-[universe500] text-[7.5vw] text-center uppercase leading-[6.5vw] pt-4">
+                Pour Tout Voir
+              </h2>
+              <img
+                className="h-20 w-56 object-cover shrink-0 rounded-full"
+                src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+              ></img>
+              <h2 className="whitespace-nowrap font-[universe500] text-[7.5vw] text-center uppercase leading-[6.5vw] pt-4">
+                Pour Tout Voir
+              </h2>
+              <img
+                className="h-20 w-56 object-cover shrink-0 rounded-full"
+                src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+              ></img>
+            </div>
+            <div className="moveX flex items-center gap-4">
+              <h2 className="whitespace-nowrap font-[universe500] text-[7.5vw] text-center uppercase leading-[6.5vw] pt-4">
+                Pour Tout Voir
+              </h2>
+              <img
+                className="h-20 w-56 object-cover shrink-0 rounded-full"
+                src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+              ></img>
+              <h2 className="whitespace-nowrap font-[universe500] text-[7.5vw] text-center uppercase leading-[6.5vw] pt-4">
+                Pour Tout Voir
+              </h2>
+              <img
+                className="h-20 w-56 object-cover shrink-0 rounded-full"
+                src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+              ></img>
+            </div>
+          </div>
+        </div>
+        <div className="link relative border-t-[1px] border-b-[1px] cursor-pointer">
+          <h1 className="font-[universe500] text-[7.5vw] text-center uppercase leading-[6.5vw] pt-4">
+            Projects
+          </h1>
+          <div className="movelink absolute top-0 flex gap-4 bg-[#C084FC] text-black">
+            <div className="moveX flex items-center gap-4">
+              <h2 className="whitespace-nowrap font-[universe500] text-[7.5vw] text-center uppercase leading-[6.5vw] pt-4">
+                Pour Tout Voir
+              </h2>
+              <img
+                className="h-20 w-56 object-cover shrink-0 rounded-full"
+                src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+              ></img>
+              <h2 className="whitespace-nowrap font-[universe500] text-[7.5vw] text-center uppercase leading-[6.5vw] pt-4">
+                Pour Tout Voir
+              </h2>
+              <img
+                className="h-20 w-56 object-cover shrink-0 rounded-full"
+                src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+              ></img>
+            </div>
+            <div className="moveX flex items-center gap-4">
+              <h2 className="whitespace-nowrap font-[universe500] text-[7.5vw] text-center uppercase leading-[6.5vw] pt-4">
+                Pour Tout Voir
+              </h2>
+              <img
+                className="h-20 w-56 object-cover shrink-0 rounded-full"
+                src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+              ></img>
+              <h2 className="whitespace-nowrap font-[universe500] text-[7.5vw] text-center uppercase leading-[6.5vw] pt-4">
+                Pour Tout Voir
+              </h2>
+              <img
+                className="h-20 w-56 object-cover shrink-0 rounded-full"
+                src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+              ></img>
+            </div>
+          </div>
         </div>
       </div>
     </div>
